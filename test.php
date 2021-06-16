57ccd75d586bfc4c41a1a5328977342d36e00922
SkullCrunchaaa <84836917+SkullCrunchaaa@users.noreply.github.com>
2021-06-16 10:44:48 +0200

    Create test.php

---
+++ test.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>PHP Test</title>
+
+    </head>
+
+    <body>
+          <?php
+      echo "Hello world";
+      ?>
+    </body>
+</html>
